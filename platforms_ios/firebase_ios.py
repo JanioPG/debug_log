a1eacfee509b8f1b88a336bfae41d67e16e56639
janiopg <janiopg@outlook.com>
2022-12-09 08:36:55 -0300

    add filters for firebase

--- platforms_ios/firebase_ios.py
+++ platforms_ios/firebase_ios.py
@@ -52,7 +52,74 @@ def no_arguments():
                 pass
 
 
+def with_arguments(args: argparse.Namespace):
+    """Filtra os logs detalhados com base nos argumentos passados pelo usuário.
 
+    Args:
+        args (argparse.Namespace): Argumentos passados pelo usuário na chamada para execução do script.
+    """
+    if args.term1 == None and args.term2 == None: # caso exista somente -v
+        no_arguments()
+
+    elif args.term1 != None and args.term2 != None:
+        proc = enable_verbose_logging()
+        re_terms = re.compile(rf"{args.term1}|{args.term2}")
+        re_capture_bundle = re.compile(r"Logging\ event")
+        new_event = re.compile(r"\d\d:\d\d:\d\d.\d\d.*0x")
+        continue_log = False
+        event_log = ""        
+
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            if continue_log == True and not new_event.search(line, re.IGNORECASE):
+                event_log += line
+            
+            elif re_capture_bundle.search(line, re.IGNORECASE) and continue_log == False:
+                event_log += line
+                continue_log = True
+
+            else:
+                if re_capture_bundle.search(event_log, re.IGNORECASE):
+                    continue_log = False
+                    event_log = re.sub(r"0x.*event:\ ", r"", event_log)
+                    
+                    check_terms = list(set(re_terms.findall(event_log, re.IGNORECASE)))
+            
+                    if len(check_terms) == 2:
+                        check_terms.sort() # sort - ordem alfabetica
+
+                        event_log = re.sub(f"{check_terms[0]}", f"\033[1;32;40m{check_terms[0]}\033[m", event_log)
+                        event_log = re.sub(f"{check_terms[1]}", f"\033[1;34;40m{check_terms[1]}\033[m", event_log)
+                        
+                        print(event_log)
+                    event_log = ""
+
+    elif args.term1 != None or args.term2 != None:
+        proc = enable_verbose_logging()
+        term = args.term1 if args.term1 != None else args.term2
+        re_terms = re.compile(rf"{term}")
+
+        re_capture_bundle = re.compile(r"Logging\ event")
+        new_event = re.compile(r"\d\d:\d\d:\d\d.\d\d.*0x")
+        continue_log = False
+        event_log = ""      
+        
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            if continue_log == True and not new_event.search(line, re.IGNORECASE):
+                event_log += line
+            
+            elif re_capture_bundle.search(line, re.IGNORECASE) and continue_log == False:
+                event_log += line
+                continue_log = True
+            else:
+                if re_capture_bundle.search(event_log, re.IGNORECASE):
+                    continue_log = False
+                    event_log = re.sub(r"0x.*event:\ ", r"", event_log)
+
+                    match = re_terms.search(event_log, re.IGNORECASE)         
+                    if match:
+                        event_log = re.sub(match.group(), f"\033[1;32;40m{match.group()}\033[m", event_log)
+                        print(event_log)
+                    event_log = ""
 
 if __name__ == "__main__":
     no_arguments()
